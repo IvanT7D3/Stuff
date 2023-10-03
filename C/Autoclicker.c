27e5f1a18cd94b89f8444844d0b830addb841eb5
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2023-10-03 16:06:31 +0200

    Create Autoclicker.c

---
+++ C/Autoclicker.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+	printf("Press Z to enable, X to disable, C to pause\n");
+	_Bool Click = 0;
+	int ClickNum = 0;
+	while (1)
+	{
+		if (GetAsyncKeyState('Z'))
+		{
+			Click = 1;
+		}
+		else if (GetAsyncKeyState('X'))
+		{
+			Click = 0;
+			break;
+		}
+		else if(GetAsyncKeyState('C'))
+		{
+			Click = 0;
+		}
+		if (Click == 1)
+		{
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			ClickNum++;
+			Sleep(100);
+		}
+	}
+	printf("Total Clicks : %d\n", ClickNum);
+	Sleep(5);
+	return 0;
+}
