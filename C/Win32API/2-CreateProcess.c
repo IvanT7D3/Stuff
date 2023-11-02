a4917013bb04cb8770d84c39b35b5f5523a0af13
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2023-11-02 15:38:53 +0100

    Update 2-CreateProcess.c

--- C/Win32API/2-CreateProcess.c
+++ C/Win32API/2-CreateProcess.c
@@ -3,18 +3,22 @@
 
 int main()
 {
-	HANDLE CreateProc = NULL;
-	STARTUPINFOW si;
+	BOOL CreateProc = 0;
+	STARTUPINFOW si = {0};
 	PROCESS_INFORMATION pi;
 
 	CreateProc = CreateProcessW(L"C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE, BELOW_NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
-	
-	if (CreateProc != NULL)
+
+	if (CreateProc == 0)
 	{
-		printf("[+] Process created With PID: %ld\n", pi.dwProcessId);
-		return EXIT_SUCCESS;
+		printf("[-] Process can't be created... Error: %ld\n", GetLastError());
+		return EXIT_FAILURE;
 	}
 
-	printf("[-] Process can't be created... Error: %ld\n", GetLastError());
-	return EXIT_FAILURE;
+	printf("[+] Process created With PID: %ld\n", pi.dwProcessId);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return EXIT_SUCCESS;
 }
