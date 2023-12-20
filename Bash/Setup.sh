fe7fc8908f402edbe2a16048675254ea215715c5
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2023-12-20 18:26:43 +0100

    Update Setup.sh

--- Bash/Setup.sh
+++ Bash/Setup.sh
@@ -26,6 +26,7 @@ apt-get install ark -y
 apt-get install tor -y
 apt-get install virtualbox -y
 apt-get install gobuster -y
+apt-get install wifiphisher -y
 apt-get install steghide -y
 apt-get install xxd -y
 apt-get install hexedit -y
@@ -33,6 +34,9 @@ apt-get install seclists -y
 apt-get install exifcleaner -y
 apt-get install openjdk-11-jdk-headless -y
 apt-get install poppler-utils -y
+apt-get install mosquitto mosquitto-clients -y
+apt-get install sshuttle -y
+apt-get install chisel -y
 pip3 install updog
 #rustscan
 #https://github.com/longld/peda
