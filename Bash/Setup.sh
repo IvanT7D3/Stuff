5e434b4585d1f4c552dbb8461f091216474b66cf
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2024-04-01 15:51:16 +0200

    Update Setup.sh

--- Bash/Setup.sh
+++ Bash/Setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#Kali Linux Basic Setup
 
 Color_Off='\033[0m'
 Red='\033[1;31m'
@@ -10,34 +9,11 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-apt-get update
+apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
 apt-get autoremove -y
-apt-get clean -y
-apt-get install libreoffice -y
-apt-get install gdb -y
-apt-get install htop -y
-apt-get install strace -y
-apt-get install ltrace -y
-apt-get install vlc -y
-apt-get install gwenview -y
-apt-get install ark -y
-apt-get install tor -y
-apt-get install virtualbox -y
-apt-get install gobuster -y
-apt-get install wifiphisher -y
-apt-get install steghide -y
-apt-get install xxd -y
-apt-get install hexedit -y
-apt-get install seclists -y
-#apt-get install exifcleaner -y
-apt-get install openjdk-11-jdk-headless -y
-apt-get install poppler-utils -y
-apt-get install mosquitto mosquitto-clients -y
-apt-get install sshuttle -y
-apt-get install chisel -y
+apt-get install htop gdb strace ltrace vlc gwenview ark tor virtualbox wifiphisher gobuster feroxbuster steghide xxd hexedit seclists openjdk-11-jdk.headless poppler-utils mosquitto mosquitto-clients sshuttle chisel ghidra golang bloodhound neo4j
 pip3 install updog
-#rustscan
-#https://github.com/longld/peda
+pip3 install bpytop
 echo -e "${Green}All done. Please restart!${Color_Off}"
