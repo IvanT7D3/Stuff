e0614666ee268cf664fe0c5486bd7f1f0761b825
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2025-05-11 10:57:56 +0200

    Update Setup.sh

--- Bash/Setup.sh
+++ Bash/Setup.sh
@@ -9,8 +9,8 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y; apt-get autoclean
-apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential htop gdb strace ltrace vlc gwenview ark tor virtualbox wifiphisher gobuster feroxbuster steghide xxd lynx hexedit seclists openjdk-11-jdk.headless poppler-utils mosquitto mosquitto-clients sshuttle chisel ghidra golang bloodhound neo4j -y
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get autoclean -y
+apt-get install python3 python3-pwntools python3-pip python3-dev git libssl-dev libffi-dev build-essential htop gdb strace ltrace vlc gwenview ark tor virtualbox wifiphisher gobuster feroxbuster steghide xxd lynx hexedit seclists openjdk-11-jdk.headless poppler-utils mosquitto mosquitto-clients sshuttle chisel ghidra golang bloodhound neo4j -y
 usermod -a -G vboxusers kali
-apt install python3-pwntools
+bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 echo -e "${Green}All done. Please restart!${Color_Off}"
