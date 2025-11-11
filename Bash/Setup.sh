b4ca2239b05db46a57f4e94e09eca627e9986a17
IvanT7D3 <146664734+IvanT7D3@users.noreply.github.com>
2025-11-11 16:30:15 +0100

    Update Setup.sh

--- Bash/Setup.sh
+++ Bash/Setup.sh
@@ -9,8 +9,34 @@ if [ $EUID -ne 0 ]; then
 	exit 1
 fi
 
-apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get autoclean -y
-apt-get install python3 python3-pwntools python3-pip python3-dev git libssl-dev libffi-dev build-essential htop gdb strace ltrace vlc gwenview ark tor virtualbox wifiphisher gobuster feroxbuster steghide xxd lynx hexedit seclists openjdk-11-jdk.headless poppler-utils mosquitto mosquitto-clients sshuttle chisel ghidra golang bloodhound neo4j -y
+if ! id kali > /dev/null 2>&1 ; then
+	echo -e "${Red}User 'kali' doesn't exist!${Color_Off}"
+	exit 1
+fi
+
+cd /home/kali/Desktop
+
+echo -e "${Green}Upgrading${Color_Off}"
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoclean && apt-get autoremove -y && apt-get upgrade --fix-missing -y
+
+echo -e "${Green}Installing main stuff${Color_Off}"
+apt-get install python3 python3-pip python3-dev python3-pwntools git libssl-dev libffi-dev build-essential htop gdb strace ltrace vlc gwenview rustup docker.io jd-gui ark tor virtualbox wifiphisher gobuster feroxbuster steghide xxd lynx hexedit seclists openjdk-11-jdk.headless poppler-utils mosquitto mosquitto-clients sshuttle chisel ghidra golang bloodhound neo4j vbindiff -y
+
+echo -e "${Green}Installing bluetooth stuff${Color_Off}"
+apt-get install bluelog blueranger btscanner redfang -y
+
+echo -e "${Green}Installing canutils and modbus-cli${Color_Off}"
+apt-get install can-utils -y
+gem install modbus-cli
+
 usermod -a -G vboxusers kali
+
+git clone https://github.com/dirkjanm/PKINITtools
+git clone https://github.com/ShutdownRepo/pywhisker
+
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
+
+chown -R kali:kali *
+updatedb
+
 echo -e "${Green}All done. Please restart!${Color_Off}"
